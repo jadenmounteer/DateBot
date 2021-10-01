789a0b1125ecaad8e97df34956cdc1c6bda002db
jmounteer <mounteerjaden@gmail.com>
2021-09-30 19:56:45 -0600

    Fixed relative paths for DateBot's different gifs

--- js/dateBotController.js
+++ js/dateBotController.js
@@ -20,35 +20,35 @@ const dateBot = {
     Changes the gif of dateBot to the talking gif
     */
     talk: function() {
-        this.dateBotImg.setAttribute("src", "assets/DateBot-talking.gif"); 
+        this.dateBotImg.setAttribute("src", "../assets/DateBot-talking.gif"); 
     },
 
     /*
     Changes the gif of dateBot to the smiling gif
     */
     smile: function() {
-        this.dateBotImg.setAttribute("src", "assets/DateBot-blinking-straight-face.gif"); 
+        this.dateBotImg.setAttribute("src", "../assets/DateBot-blinking-straight-face.gif"); 
     },
 
     /*
     Changes the gif of dateBot to the love it gif
     */
     loveIt: function() {
-        this.dateBotImg.setAttribute("src", "assets/DateBot-heart-animation.gif"); 
+        this.dateBotImg.setAttribute("src", "../assets/DateBot-heart-animation.gif"); 
     },
 
     /*
     Changes the gif of dateBot to the nailed it gif
     */
     nailedIt: function() {
-        this.dateBotImg.setAttribute("src", "assets/DateBot-nailed-it.gif"); 
+        this.dateBotImg.setAttribute("src", "../assets/DateBot-nailed-it.gif"); 
     },
 
     /*
     Changes the gif of dateBot to the confused it gif
     */
     confused: function() {
-        this.dateBotImg.setAttribute("src", "assets/DateBot-question-mark.gif"); 
+        this.dateBotImg.setAttribute("src", "../assets/DateBot-question-mark.gif"); 
     },
 
 }
