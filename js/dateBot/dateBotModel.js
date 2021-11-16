6a8c58e81c69f287275b662f3bbd2b4e3ef4d453
jmounteer <mounteerjaden@gmail.com>
2021-11-16 13:40:13 -0700

    Dates are now shuffled when list is generated

--- js/dateBot/dateBotModel.js
+++ js/dateBot/dateBotModel.js
@@ -119,8 +119,29 @@ export default class DateBotModel {
             
         });
 
+        // Shuffle the list of perfect dates
+        function shuffle(array) {
+            let currentIndex = array.length,  randomIndex;
+          
+            // While there remain elements to shuffle...
+            while (currentIndex != 0) {
+          
+              // Pick a remaining element...
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex--;
+          
+              // And swap it with the current element.
+              [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+            }
+          
+            return array;
+          }
+        let shuffledArray = shuffle(listOfPerfectDates);
+
+
         // Return the list of perfect dates to the user
-        return listOfPerfectDates;
+        return shuffledArray;
 
     }
 
